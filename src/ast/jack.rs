94ed3a3b235688483846e029dbb90fa22aae9b78
JustinGrubbs <grubbsjr@hendrix.edu>
2025-02-22 18:19:34 -0500

    Setup for creating Jack parser

--- src/ast/jack.rs
+++ src/ast/jack.rs
@@ -4,28 +4,33 @@
 //      The compilation unit is a class. A class is a sequence of tokens, as follows:
 
 // 'class' [className] '{' [classVarDec*] [subRoutineDec*] '}'
+#[derive(Debug,Clone)]
 pub struct Class {
-    class_name: String,
-    class_dec: ClassDec,
+    pub class_name: String,
+    pub class_dec: ClassDec,
 }
 
+#[derive(Debug,Clone)]
 pub struct ClassDec {
-    class_var_dec: Vec<ClassVarDec>,
-    subroutine_dec: Vec<SubroutineDec>,
+    pub class_var_dec: Vec<ClassVarDec>,
+    pub subroutine_dec: Vec<SubroutineDec>,
 }
 
 // ('static' | 'field') [type] [varName* sepBy ','] ';'
+#[derive(Debug,Clone)]
 pub struct ClassVarDec {
-    class_var_type: ClassVarType,
-    r#type: Type,
-    vars: Vec<String>,
+    pub class_var_type: ClassVarType,
+    pub r#type: Type,
+    pub vars: Vec<String>,
 }
 
+#[derive(Debug,Clone)]
 pub enum ClassVarType {
     Static,
     Field,
 }
 
+#[derive(Debug,Clone)]
 pub enum Type {
     Int,
     Char,
@@ -34,45 +39,52 @@ pub enum Type {
 }
 
 // ('constructor' | 'function' | 'method') ('void' | [type]) [subroutineName] '(' [parameterList] ')' [subroutineBody]
+#[derive(Debug,Clone)]
 pub struct SubroutineDec {
-    subroutine_type: SubroutineType,
-    subroutine_return_type: SubroutineReturnType,
-    subroutine_name: String,
-    parameter_list: Vec<Parameter>,
-    subroutine_body: Vec<SubroutineBody>,
+    pub subroutine_type: SubroutineType,
+    pub subroutine_return_type: SubroutineReturnType,
+    pub subroutine_name: String,
+    pub parameter_list: Vec<Parameter>,
+    pub subroutine_body: Vec<SubroutineBody>,
 }
 
+#[derive(Debug,Clone)]
 pub enum SubroutineType {
     Constructor,
     Function,
     Method,
 }
 
+#[derive(Debug,Clone)]
 pub enum SubroutineReturnType {
     Void,
     Type(Type),
 }
 
+#[derive(Debug,Clone)]
 pub struct Parameter {
-    r#type: Type,
-    var_name: String,
+    pub r#type: Type,
+    pub var_name: String,
 }
 
 // '{' [varDec*] [statement*] '}'
+#[derive(Debug,Clone)]
 pub enum SubroutineBody {
     VarDec(VarDec),
     Statement(Statement),
 }
 
 // 'var' [type] [varName] (',' [varName])* ';'
+#[derive(Debug,Clone)]
 pub struct VarDec{
-    r#type: Type,
-    var_name: Vec<String>,
+    pub r#type: Type,
+    pub var_name: Vec<String>,
 }
 
 
 // Statements:
 
+#[derive(Debug,Clone)]
 pub enum Statement {
     LetStatement(LetStatement),
     IfStatement(IfStatement),
@@ -81,39 +93,46 @@ pub enum Statement {
     ReturnStatement(ReturnStatement),
 }
 
+#[derive(Debug,Clone)]
 pub struct LetStatement {
-    var_name: String,
-    option_expression: Option<Expression>,
-    expression: Expression,
+    pub var_name: String,
+    pub option_expression: Option<Expression>,
+    pub expression: Expression,
 }
 
+#[derive(Debug,Clone)]
 pub struct IfStatement {
-    r#if: Expression,
-    then: Vec<Statement>,
-    r#else: Option<Vec<Statement>>, 
+    pub r#if: Expression,
+    pub then: Vec<Statement>,
+    pub r#else: Option<Vec<Statement>>, 
 }
 
+#[derive(Debug,Clone)]
 pub struct WhileStatement {
-    case: Expression,
-    body: Vec<Statement>,
+    pub case: Expression,
+    pub body: Vec<Statement>,
 }
 
+#[derive(Debug,Clone)]
 pub struct DoStatement {
-    call: SubroutineCall,
+    pub call: SubroutineCall,
 }
 
+#[derive(Debug,Clone)]
 pub struct ReturnStatement {
-    r#return: Option<Expression>,
+    pub r#return: Option<Expression>,
 }
 
 
 // Expressions:
 
+#[derive(Debug,Clone)]
 pub enum Expression {
     Expr(Term),
     Bin(Term,BinaryOp,Term),
 }
 
+#[derive(Debug,Clone)]
 pub enum Term {
     IntegerConstant(i16),
     StringConstant(String),
@@ -124,11 +143,13 @@ pub enum Term {
     SubroutineCall(SubroutineCall),
 }
 
+#[derive(Debug,Clone)]
 pub enum SubroutineCall {
     Call(String, Vec<Expression>),
     ClassCall(String, String, Vec<Expression>),
 }
 
+#[derive(Debug,Clone)]
 pub enum BinaryOp {
     Plus,
     Minus,
@@ -141,11 +162,13 @@ pub enum BinaryOp {
     Equal,
 }
 
+#[derive(Debug,Clone)]
 pub enum UnaryOp {
     Negation,
     Tilde,
 }
 
+#[derive(Debug,Clone)]
 pub enum Keyword {
     True,
     False,
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,47 @@
-use crate::lexer;
+use crate::lexer::*;
+use crate::ast::jack::*;
 use chumsky::prelude::*;
 
 
+
+fn kw(expected: crate::lexer::Keyword) -> impl Parser<Token, (), Error = Simple<Token>> {
+    just(Token::Keyword(expected)).ignored()
+}
+
+fn sym(expected: Symbol) -> impl Parser<Token, (), Error = Simple<Token>> {
+    just(Token::Symbol(expected)).ignored()
+}
+
+fn ident() -> impl Parser<Token, String, Error = Simple<Token>> {
+    select! {
+        Token::Identifier(s) => s,
+    }
+}
+
+fn int_const() -> impl Parser<Token, i16, Error = Simple<Token>> {
+    select! {
+        Token::Integer(i) => i,
+    }
+}
+
+fn string_const() -> impl Parser<Token, String, Error = Simple<Token>> {
+    select! {
+        Token::String(s) => s,
+    }
+}
+
+
+
+
+
+
+fn parse_type() -> impl Parser<Token, Type, Error = Simple<Token>> {
+    choice((
+        kw(crate::lexer::Keyword::Int).to(Type::Int),
+        kw(crate::lexer::Keyword::Boolean).to(Type::Boolean),
+        kw(crate::lexer::Keyword::Char).to(Type::Char),
+        ident().map(|s| Type::ClassName(s))
+    ))
+}
 
 
