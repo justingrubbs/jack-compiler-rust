ca665b81fc5b82fd898739a1dad414a8e29df478
Justin Grubbs <grubbsjr@hendrix.edu>
2025-02-25 17:55:28 -0500

    Organized pretty_printer

--- src/pretty_printer.rs
+++ src/pretty_printer.rs
@@ -39,6 +39,36 @@ impl PrettyPrint for ClassDec {
     }
 }
 
+impl PrettyPrint for ClassVarDec {
+    fn pretty_print(&self, i: usize) -> String {
+        let class_var_type = self.class_var_type.pretty_print(i);
+        let r#type = self.r#type.pretty_print(i);
+        let vars = self.vars.iter().map(|v| v.to_string()).join(", ");
+
+        format!("{}{} {} {};", tab(i), class_var_type, r#type, vars)
+    }
+}
+
+impl PrettyPrint for ClassVarType {
+    fn pretty_print(&self, _: usize) -> String {
+        match self {
+            ClassVarType::Static => "static".to_string(),
+            ClassVarType::Field => "field".to_string(),
+        }
+    }
+}
+
+impl PrettyPrint for Type {
+    fn pretty_print(&self, _: usize) -> String {
+        match self {
+            Type::Int => "int".to_string(),
+            Type::Char => "char".to_string(),
+            Type::Boolean => "boolean".to_string(),
+            Type::ClassName(s) => s.to_string(),
+        }
+    }
+}
+
 impl PrettyPrint for SubroutineDec {
     fn pretty_print(&self, i: usize) -> String {
         let subroutine_type = self.subroutine_type.pretty_print(i);
@@ -53,8 +83,6 @@ impl PrettyPrint for SubroutineDec {
 
         let subroutine_body = self.subroutine_body.pretty_print(i + 1);
 
-        // removing this newline
-        // format!("{}{} {} {}({}) {{{}\n{}}}",
         format!(
             "{}{} {} {}({}) {{\n{}\n{}}}",
             tab(i),
@@ -64,6 +92,35 @@ impl PrettyPrint for SubroutineDec {
             parameter_list,
             subroutine_body,
             tab(i)
+        )
+    }
+}
+
+impl PrettyPrint for SubroutineType {
+    fn pretty_print(&self, _: usize) -> String {
+        match self {
+            SubroutineType::Constructor => "constructor".to_string(),
+            SubroutineType::Function => "function".to_string(),
+            SubroutineType::Method => "method".to_string(),
+        }
+    }
+}
+
+impl PrettyPrint for SubroutineReturnType {
+    fn pretty_print(&self, i: usize) -> String {
+        match self {
+            SubroutineReturnType::Void => "void".to_string(),
+            SubroutineReturnType::Type(t) => t.pretty_print(i),
+        }
+    }
+}
+
+impl PrettyPrint for Parameter {
+    fn pretty_print(&self, i: usize) -> String {
+        format!(
+            "{} {}",
+            self.r#type.pretty_print(i),
+            self.var_name.to_string()
         )
     }
 }
@@ -85,6 +142,16 @@ impl PrettyPrint for SubroutineBody {
         format!("{}{}", var_decs, statements)
     }
 }
+
+impl PrettyPrint for VarDec {
+    fn pretty_print(&self, i: usize) -> String {
+        let r#type = self.r#type.pretty_print(i);
+        let var_names = self.var_name.iter().map(|vn| vn.to_string()).join(", ");
+
+        format!("{} {}", r#type, var_names)
+    }
+}
+
 impl PrettyPrint for Statement {
     fn pretty_print(&self, i: usize) -> String {
         match self {
@@ -137,21 +204,6 @@ impl PrettyPrint for Statement {
     }
 }
 
-impl PrettyPrint for SubroutineCall {
-    fn pretty_print(&self, i: usize) -> String {
-        match self {
-            SubroutineCall::ClassCall(s1, s2, es) => {
-                let exprs = es.iter().map(|e| e.pretty_print(i)).join(", ");
-                format!("{}.{}({})", s1, s2, exprs)
-            }
-            SubroutineCall::Call(s, es) => {
-                let exprs = es.iter().map(|e| e.pretty_print(i)).join(", ");
-                format!("{}({})", s, exprs)
-            }
-        }
-    }
-}
-
 impl PrettyPrint for Expression {
     fn pretty_print(&self, i: usize) -> String {
         let term = self.term.pretty_print(i);
@@ -184,22 +236,17 @@ impl PrettyPrint for Term {
     }
 }
 
-impl PrettyPrint for UnaryOp {
-    fn pretty_print(&self, _: usize) -> String {
+impl PrettyPrint for SubroutineCall {
+    fn pretty_print(&self, i: usize) -> String {
         match self {
-            UnaryOp::Negation => "-".to_string(),
-            UnaryOp::Tilde => "~".to_string(),
-        }
-    }
-}
-
-impl PrettyPrint for KeywordConstant {
-    fn pretty_print(&self, _: usize) -> String {
-        match self {
-            KeywordConstant::True => "true".to_string(),
-            KeywordConstant::False => "false".to_string(),
-            KeywordConstant::Null => "null".to_string(),
-            KeywordConstant::This => "this".to_string(),
+            SubroutineCall::ClassCall(s1, s2, es) => {
+                let exprs = es.iter().map(|e| e.pretty_print(i)).join(", ");
+                format!("{}.{}({})", s1, s2, exprs)
+            }
+            SubroutineCall::Call(s, es) => {
+                let exprs = es.iter().map(|e| e.pretty_print(i)).join(", ");
+                format!("{}({})", s, exprs)
+            }
         }
     }
 }
@@ -220,70 +267,22 @@ impl PrettyPrint for BinaryOp {
     }
 }
 
-impl PrettyPrint for VarDec {
-    fn pretty_print(&self, i: usize) -> String {
-        let r#type = self.r#type.pretty_print(i);
-        let var_names = self.var_name.iter().map(|vn| vn.to_string()).join(", ");
-
-        format!("{} {}", r#type, var_names)
-    }
-}
-
-impl PrettyPrint for SubroutineType {
+impl PrettyPrint for UnaryOp {
     fn pretty_print(&self, _: usize) -> String {
         match self {
-            SubroutineType::Constructor => "constructor".to_string(),
-            SubroutineType::Function => "function".to_string(),
-            SubroutineType::Method => "method".to_string(),
+            UnaryOp::Negation => "-".to_string(),
+            UnaryOp::Tilde => "~".to_string(),
         }
     }
 }
 
-impl PrettyPrint for SubroutineReturnType {
-    fn pretty_print(&self, i: usize) -> String {
-        match self {
-            SubroutineReturnType::Void => "void".to_string(),
-            SubroutineReturnType::Type(t) => t.pretty_print(i),
-        }
-    }
-}
-
-impl PrettyPrint for Parameter {
-    fn pretty_print(&self, i: usize) -> String {
-        format!(
-            "{} {}",
-            self.r#type.pretty_print(i),
-            self.var_name.to_string()
-        )
-    }
-}
-
-impl PrettyPrint for ClassVarDec {
-    fn pretty_print(&self, i: usize) -> String {
-        let class_var_type = self.class_var_type.pretty_print(i);
-        let r#type = self.r#type.pretty_print(i);
-        let vars = self.vars.iter().map(|v| v.to_string()).join(", ");
-
-        format!("{}{} {} {};", tab(i), class_var_type, r#type, vars)
-    }
-}
-
-impl PrettyPrint for ClassVarType {
+impl PrettyPrint for KeywordConstant {
     fn pretty_print(&self, _: usize) -> String {
         match self {
-            ClassVarType::Static => "static".to_string(),
-            ClassVarType::Field => "field".to_string(),
-        }
-    }
-}
-
-impl PrettyPrint for Type {
-    fn pretty_print(&self, _: usize) -> String {
-        match self {
-            Type::Int => "int".to_string(),
-            Type::Char => "char".to_string(),
-            Type::Boolean => "boolean".to_string(),
-            Type::ClassName(s) => s.to_string(),
+            KeywordConstant::True => "true".to_string(),
+            KeywordConstant::False => "false".to_string(),
+            KeywordConstant::Null => "null".to_string(),
+            KeywordConstant::This => "this".to_string(),
         }
     }
 }
